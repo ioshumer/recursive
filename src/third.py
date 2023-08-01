e59d183394339ea6945036a272497802d9edfa86
IO Shumer <ioshumer@gmail.com>
2023-08-01 18:50:52 +0300

    third

--- src/third.py
+++ src/third.py
@@ -1,26 +1,7 @@
-class CustomList:
-    def __init__(self, list_of_values):
-        self._values: list = list_of_values
-
-    def pop(self):
-        if len(self._values):
-            return self._values.pop()
-        else:
-            return None
-
-    def _define_len(self):
-        if len(self._values) == 0:
-            return 0
-        else:
-            self.pop()
-            sum = 1 + self._define_len()
-            return sum
-
-    def __len__(self):
-        list_of_values = self._values.copy()
-        length = self._define_len()
-        self._values = list_of_values
-        return length
-
-    def len(self):
-        return len(self)
+def define_length(list_):
+    if len(list_) == 0:
+        return 0
+    else:
+        list_.pop()
+        sum = 1 + define_length(list_)
+        return sum
--- tests/test_third.py
+++ tests/test_third.py
@@ -1,1 +1,16 @@
 import pytest
+
+from src.third import define_length
+
+
+@pytest.mark.parametrize(
+    ('list_', 'result'),
+    [
+        ([], 0),
+        ([1], 1),
+        ([1, 2], 2),
+        ([1, 2, 3], 3)
+    ]
+)
+def test_fourt(list_, result):
+    assert define_length(list_) == result
